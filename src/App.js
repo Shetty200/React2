40d2b975cbb986f0a35557120a4648af2f5046e6
harshitha <Shetty/200>
2023-01-05 16:55:18 +0530

    form updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'block', 
+                  width: 700, 
+                  padding: 30 }}>
+      <h4>Fill the Form Details</h4>
+      <Form>
+      <Form.Group>
+          <Form.Label>Enter your  name:</Form.Label>
+          <Form.Control type="text" 
+                        placeholder="Enter your full name" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Enter your email Id</Form.Label>
+          <Form.Control type="email" 
+                        placeholder="Enter your your email address" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Enter your age:</Form.Label>
+          <Form.Control type="number" placeholder="Enter your age" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+           Submit
+        </Button>
+      </Form>
     </div>
   );
 }
-
-export default App;
